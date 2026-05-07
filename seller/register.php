9f905f0b7ba4d24bdff42d6a6e6cffea21b18095
Xifei Wu <2024213908@bupt.cn>
2026-05-07 21:28:23 +0800

    git commit -m "feat(register): implement strict regular expression for registration"

--- seller/register.php
+++ seller/register.php
@@ -27,48 +27,51 @@
             
             <div class="left-gray-box">
                 <h2 class="box-main-title"> Seller Registration </h2>
-                
-                <form id="RegistrationForm" style="margin-top: 20px;">
+                <form id="RegistrationForm" action="register.php" method="POST" onSubmit="return checkForm();" style="margin-top: 20px;">
                     
                     <div class="input-field-group">
                         <label> Name: </label>
-                        <input type="text" name="name" required pattern="[a-zA-Z\s]+" title="Only alphabetical letters and space allowed" class="box-input-field">
+                        <input type="text" name="name" id="name" class="box-input-field">
                     </div>
 
                     <div class="input-field-group">
                         <label> Address: </label>
-                        <input type="text" name="address" required pattern="[a-zA-Z0-9\s]+" title="Only alphabetical letters and space allowed" class="box-input-field">
+                        <input type="text" name="address" id="address" class="box-input-field">
                     </div>
 
                     <div class="input-field-group">
                         <label> Phone number: </label>
-                        <input type="text" name="phone" required pattern="^1[0-9][0-9]{9}$" title="Please enter a valid 11-digit China phone number" class="box-input-field">
+                        <input type="text" name="phone" id="phone" class="box-input-field">
                     </div>
 
                     <div class="input-field-group">
                         <label> Email address: </label>
-                        <input type="text" name="email" required pattern="^[a-zA-Z0-9]+@[a-zA-Z]+\.(com|cn)$" title="Must contain exactly one '@' and end with .cn or .com" class="box-input-field">
+                        <input type="text" name="email" id="email" class="box-input-field">
                     </div>
 
                     <div class="input-field-group">
                         <label> Username: </label>
-                        <input type="text" name="username" required pattern="^[a-zA-Z0-9]{6,}$" title="Must consist of at least 6 alphanumeric characters" class="box-input-field">
+                        <input type="text" name="username" id="username" class="box-input-field">
                     </div>
 
                     <div class="input-field-group">
                         <label> Password: </label>
-                        <input type="password" name="password" required pattern="^[a-zA-Z0-9]{6,}$" title="Must consist of at least 6 alphanumeric characters" class="box-input-field">
+                        <input type="password" name="password_hash" id="password_hash" class="box-input-field">
                     </div>
 
-                    <input type="submit" value="Register" style="background-color: black; color: white; padding: 15px 50px;">
+                    <input type="submit" value="Register Now !" style="background-color: black; color: white; padding: 15px 50px;">
                 </form>
-
             </div>
+
+
+
 
             <div class="right-explain-text">
                 <h1 style="font-size: 45pt;"> Join Us </h1>
                 <p style="font-size: 25pt;"> Become a seller on AutoVerve today and connect with millions of buyers nationwide. </p>
             </div>
+
+
 
         </div>
 
