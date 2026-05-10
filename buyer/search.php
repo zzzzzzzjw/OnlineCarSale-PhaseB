957d08ceaa2cceb28d047f42f5f797d4f3186aa2
lzyi06 <13125518295@163.com>
2026-05-10 11:21:02 +0800

    Remove unused model and year variables

--- buyer/search.php
+++ buyer/search.php
@@ -8,8 +8,7 @@ $colour = $_GET['colour'] ?? '';
 $price = $_GET['price'] ?? '';
 
 if ($model != "" || $year != "" || $colour != "" || $price != "") {
-    $model = $_GET['model'] ?? '';
-    $year = $_GET['year'] ?? '';
+
     $sql = "SELECT car_id, model, colour, year, location, price, image_url, description FROM cars WHERE 1=1";
     $params = [];
     $types = "";
